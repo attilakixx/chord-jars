e87a471af7090b845259057d9bebc8842c65ca46
Kiss Attila <attila.kiss@idata.hu>
2024-09-29 15:54:10 +0200

    Create Notes class

---
+++ src/chord_jars/Note.java
@@ -0,0 +1,15 @@
+package chord_jars;
+
+public class Note {
+	private final String label;
+
+	public Note(String label) {
+		super();
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+}
---
+++ src/chord_jars/Notes.java
@@ -0,0 +1,21 @@
+package chord_jars;
+
+public class Notes {
+
+	public static Note[] NOTES = {
+			new Note("A"),
+			new Note("A#"),
+			new Note("B"),
+			new Note("C"),
+			new Note("C#"),
+			new Note("D"),
+			new Note("D#"),
+			new Note("E"),
+			new Note("F"),
+			new Note("F#"),
+			new Note("G"),
+			new Note("G#")};
+
+	public String getNote(Note from, int interval, boolean up) {
+	}
+}
